dd5ce4a78668886d3e69c1faeb1b6cf408c68994
Gabriel Castro <gabrielcssg@gmail.com>
2023-04-06 11:55:59 -0300

    feat: group card component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
+import { GroupCard } from "../../components/GroupCard";
 import { Header } from "../../components/Header";
+import { Highlight } from "../../components/Highlight";
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Nome da turma" />
+      <GroupCard title="Nome da turma" />
     </Container>
   );
 }
