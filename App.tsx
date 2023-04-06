6b66266a6cb1c710720a9969f4cad70dbdf44d22
Gabriel Castro <gabrielcssg@gmail.com>
2023-04-06 11:11:27 -0300

    feat: expo fonts and app loading

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import { Groups } from "@screens/Groups";
 
 import theme from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
